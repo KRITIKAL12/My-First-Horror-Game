46a39177fdcd34d001f90f4650027984b36fe4b8
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-09-06 16:33:59 +0530

    Zombie Door Trigger w.i.p

--- My First Horror Game/Assets/Scripts/Sequences/BZJumpTrigger.cs
+++ My First Horror Game/Assets/Scripts/Sequences/BZJumpTrigger.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class BZJumpTrigger : MonoBehaviour
 {
-    
+    public AudioSource DoorBang;
+    public AudioSource DoorJumpMusic;
+    public GameObject TheZombie;
+    public GameObject TheDoor;
+
+    void OnTriggerEnter()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        TheDoor.GetComponent<Animation>().Play("JumpDoorAnim");
+        DoorBang.Play();
+        TheZombie.SetActive(true);
+    }
+
+
+
 }
