c7edcd509994b3c6faa9a0d7684c815d0c955f0d
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-09-07 00:36:13 +0530

    Zombie is moving

--- My First Horror Game/Assets/Scripts/PlayerMovement.cs
+++ My First Horror Game/Assets/Scripts/PlayerMovement.cs
@@ -25,7 +25,7 @@ public class PlayerMovement : MonoBehaviour
     {
        
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        Debug.Log(isGrounded);
+        
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
