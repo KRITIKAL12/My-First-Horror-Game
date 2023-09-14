714e67e57265d021bd9ad53b2402a912bd842934
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-09-14 23:15:51 +0530

    Ammo Included

--- My First Horror Game/Assets/Scripts/AmmoPickup.cs
+++ My First Horror Game/Assets/Scripts/AmmoPickup.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class AmmoPickup : MonoBehaviour
 {
     public GameObject theAmmo;
+    public GameObject ammoDisplayBox;
+
     void OnTriggerEnter(Collider other)
     {
+        ammoDisplayBox.SetActive(true);
+        GlobalAmmo.ammoCount += 7;
+
         theAmmo.SetActive(false);
     }
 }
