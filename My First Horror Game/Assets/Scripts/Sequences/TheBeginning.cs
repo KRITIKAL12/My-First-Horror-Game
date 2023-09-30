334b6f23c7a9f46dd0ad06221b049e874bb518ab
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-09-30 11:54:26 +0530

    Setting up intro scene

---
+++ My First Horror Game/Assets/Scripts/Sequences/TheBeginning.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheBeginning : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public GameObject FadeScreenIn;
+    public GameObject TextBox;
+    
+
+    void Start()
+    {
+        ThePlayer.GetComponent<PlayerMovement>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        FadeScreenIn.SetActive(false);
+        TextBox.GetComponent<Text>().text = "...where am I?";
+     
+        yield return new WaitForSeconds(2);
+        TextBox.GetComponent<Text>().text = "";
+        yield return new WaitForSeconds(0.5F);
+        TextBox.GetComponent<Text>().text = "I need to get out of here.";
+       
+        yield return new WaitForSeconds(1.7f);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<PlayerMovement>().enabled = true;
+    }
+}
