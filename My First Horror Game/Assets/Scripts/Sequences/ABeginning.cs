9472819b7563b08b59459de95809e3455e8601da
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-09-30 12:22:51 +0530

    Have to add Starting scene text

--- My First Horror Game/Assets/Scripts/Sequences/ABeginning.cs
+++ My First Horror Game/Assets/Scripts/Sequences/ABeginning.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TheBeginning : MonoBehaviour
+public class ABeginning : MonoBehaviour
 {
     public GameObject ThePlayer;
     public GameObject FadeScreenIn;
@@ -19,16 +19,17 @@ public class TheBeginning : MonoBehaviour
     IEnumerator ScenePlayer()
     {
         yield return new WaitForSeconds(1.5f);
-        FadeScreenIn.SetActive(false);
         TextBox.GetComponent<Text>().text = "...where am I?";
      
         yield return new WaitForSeconds(2);
         TextBox.GetComponent<Text>().text = "";
         yield return new WaitForSeconds(0.5F);
         TextBox.GetComponent<Text>().text = "I need to get out of here.";
-       
+
         yield return new WaitForSeconds(1.7f);
         TextBox.GetComponent<Text>().text = "";
         ThePlayer.GetComponent<PlayerMovement>().enabled = true;
+
+        FadeScreenIn.SetActive(false);
     }
 }
