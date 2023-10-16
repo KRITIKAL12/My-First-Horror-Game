7ac194cbe26ab2cdcb01e0280753cbf4b78383e2
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-10-16 23:29:01 +0530

    Shine added to key and axe

--- My First Horror Game/Assets/Scripts/Environment/VaseBroken.cs
+++ My First Horror Game/Assets/Scripts/Environment/VaseBroken.cs
@@ -7,9 +7,14 @@ public class VaseBroken : MonoBehaviour
     public GameObject fakeVase;
     public GameObject brokenVase;
     public GameObject sphereObject;
+    public GameObject ExtraCross;
+    public float TheDistance;
+    public AudioSource potteryBreak;
 
-
-
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
 
     public void BreakTheVase()
     {
@@ -17,12 +22,46 @@ public class VaseBroken : MonoBehaviour
         StartCoroutine(BreakVase());
     }
 
+    void OnMouseOver()
+    {
+        if (TheDistance <= 2)
+        {
+            ExtraCross.SetActive(true);
+          
+
+        }
+        if (Input.GetButtonDown("Action"))
+        {
+            if (TheDistance <= 2)
+            {
+          
+                ExtraCross.SetActive(false);
+             
+
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        ExtraCross.SetActive(false);
+    
+    }
+
+
+
     IEnumerator BreakVase()
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        potteryBreak.Play();
         fakeVase.SetActive(false);
         brokenVase.SetActive(true);
+        ExtraCross.SetActive(false);
         yield return new WaitForSeconds(0.05f);
+        sphereObject.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        sphereObject.SetActive(false);
+
     }
 
 }
