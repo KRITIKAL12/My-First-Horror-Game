cd282b9f7f31fd7df501255396aed6384d51b103
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-10-16 15:45:08 +0530

    Scene Transition Done

--- My First Horror Game/Assets/Scripts/CellExitDoor.cs
+++ My First Horror Game/Assets/Scripts/CellExitDoor.cs
@@ -1,18 +1,16 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
-public class CellExitDoor : MonoBehaviour
+public class DoorCellOpen : MonoBehaviour
 {
     public float TheDistance;
     public GameObject ActionDisplay;
     public GameObject ActionText;
-    //public GameObject TheDoor;
+    public GameObject TheDoor;
     public AudioSource CreakSound;
     public GameObject ExtraCross;
-    public GameObject fadeOut;
 
     // Update is called once per frame
     void Update()
@@ -35,9 +33,7 @@ public class CellExitDoor : MonoBehaviour
                 this.GetComponent<BoxCollider>().enabled = false;
                 ActionDisplay.SetActive(false);
                 ActionText.SetActive(false);
-                fadeOut.SetActive(true);
-                StartCoroutine(FadeToExit());
-                //TheDoor.GetComponent<Animation> ().Play("FirstDoorOpenAnim");
+                TheDoor.GetComponent<Animation> ().Play("FirstDoorOpenAnim");
                 CreakSound.Play();
             }
         }
@@ -48,12 +44,6 @@ public class CellExitDoor : MonoBehaviour
         ExtraCross.SetActive(false);
         ActionDisplay.SetActive(false);
         ActionText.SetActive(false);
-    }
-
-    IEnumerator FadeToExit()
-    {
-        yield return new WaitForSeconds(3);
-        SceneManager.LoadScene(3);
     }
 
 }
---
+++ My First Horror Game/Assets/Scripts/Environment/VaseBroken.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaseBroken : MonoBehaviour
+{
+    public GameObject fakeVase;
+    public GameObject brokenVase;
+    public GameObject sphereObject;
+
+    public void DamageZombie(int DamageAmount)
+    {
+        StartCoroutine(BreakVase());
+    }
+
+    IEnumerator BreakVase()
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        fakeVase.SetActive(false);
+        brokenVase.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+    }
+
+}
